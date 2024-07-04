2cf1bb178c21e543b468d07607b85876692591ac
simon njoroge <mukirisimon22@gmail.com>
2024-07-04 16:40:04 +0300

    vehicle rental api

--- src/navbar_table/navbarrouter.ts
+++ src/navbar_table/navbarrouter.ts
@@ -2,7 +2,7 @@ import {Hono} from "hono"
 import {navbarservices,getnavbar,createnavbar,updatenavbar,deletenavbar} from './navbar.controller'
 
 export const navbarrouter=new Hono();
-navbarrouter.get("/paymentall",navbarservices)
+navbarrouter.get("/navbarall",navbarservices)
 navbarrouter.get("/navbar/:id",getnavbar)
 navbarrouter.post("/navbaradd",createnavbar)
 navbarrouter.put("/updatenavbar/:id",updatenavbar)
