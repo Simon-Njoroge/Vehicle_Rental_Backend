74867e9fb21cfc803215e2261326bc73277df578
simon njoroge <mukirisimon22@gmail.com>
2024-07-23 10:40:19 +0300

    vehicle rental backend

--- src/about/aboutrouter.ts
+++ src/about/aboutrouter.ts
@@ -2,7 +2,7 @@ import {Hono} from "hono"
 import {aboutservices,getabout,createabouts,updateabouts,deleteabouts} from './aboutcontroller'
 
 export const aboutrouters=new Hono();
-aboutrouters.get("/aboutall",aboutservices)
+aboutrouters.get("/aboutleaders",aboutservices)
 aboutrouters.get("/about/:id",getabout)
 aboutrouters.post("/aboutadd",createabouts)
 aboutrouters.put("/updateabout/:id",updateabouts)
